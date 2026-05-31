1135910af178823e06df54a0ef0fad786ff23ff5
meghanaRB <38452628+meghanaRB@users.noreply.github.com>
2026-05-30 21:00:45 -0400

    Problem 2 -- not finished week 5

---
+++ slidingWIndowMaximum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        
+        
+        outputs = []
+        for i in range(len(nums)-k+1):
+
+            # print(nums[i:i+k])
+            x = max(nums[i:i+k])
+
+            outputs.append(x)
+
+
+        # print(outputs)
+        return outputs
+
+
+##brute force -- nexts to be modified
