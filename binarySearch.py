6f506b13c75f718490e8fea9859c54f85f8712d0
meghanaRB <38452628+meghanaRB@users.noreply.github.com>
2026-06-17 23:38:08 -0400

    Problem 2  Week 6 Jun 14th to 212026

---
+++ binarySearch.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        lo, hi = 0, len(nums)-1
+         
+        while lo<=hi:
+
+            mid = (lo + hi)//2
+
+            if target==nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                lo = mid + 1
+            else:
+                hi = mid -1
+        
+        return -1
